2ada53a0da1b0edc2e31ced8060b3deb16964a23
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-27 22:48:59 +0300

    recreate board

--- knight/js/knight.js
+++ knight/js/knight.js
@@ -12,84 +12,274 @@ function display(){
 
   };
 };
+// // get valid coordinates
+// function validCoordinates (arr, a, b){
+//   if(a > 8 || a < 0 || b > 8 || b > 0){
+//     return false;
+//   }
+//   for(let i =0; i < arr.length; i++){
+//     if(arr[i][0] === a && arr [i][i] === b){
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// // validCoordinates();
 
-// const myBoard = () => {
+// function chessTable (){
+//   const center = document.createElement("center");
+//   const table = document.createElement("table");
 //   for (let i = 1; i < 9; i++) {
-//     const square = document.createElement("div");
-//     if (i % 2 === 0) {
-//       square.style.backgroundColor = "green";
-//     } else {
-//       square.style.backgroundColor = "white";
-//     }
-//     chessBoard.appendChild(square);
-
-//   }
-//   for (let i = 1; i < 9; i++) {
-//     const square1 = document.createElement("div");
-//     if (i % 2 === 0) {
-//       square1.style.backgroundColor = "white";
-//     } else {
-//       square1.style.backgroundColor = "green";
-//     }
-//     chessBoard.appendChild(square1);
-//   }
-//   for (let i = 1; i < 9; i++) {
-//     const square2 = document.createElement("div");
-//     if (i % 2 === 0) {
-//       square2.style.backgroundColor = "green";
-//     } else {
-//       square2.style.backgroundColor = "white";
-//     }
-//     chessBoard.appendChild(square2);
-//   }
-//   for (let i = 1; i < 9; i++) {
-//     const square3 = document.createElement("div");
-//     if (i % 2 === 0) {
-//       square3.style.backgroundColor = "white";
-//     } else {
-//       square3.style.backgroundColor = "green";
-//     }
-//     chessBoard.appendChild(square3);
-//   }
-//   for (let i = 1; i < 9; i++) {
-//     const square4 = document.createElement("div");
-//     if (i % 2 === 0) {
-//       square4.style.backgroundColor = "green";
-//     } else {
-//       square4.style.backgroundColor = "white";
-//     }
-//     chessBoard.appendChild(square4);
-//   }
-//   let i= 1;
-//    for (let i = 1; i < 9; i++) {
-//      const square5 = document.createElement("div");
-//      if (i % 2 === 0) {
-//        square5.style.backgroundColor = "white";
-//      } else {
-//        square5.style.backgroundColor = "green";
-//      }
-//      chessBoard.appendChild(square5);
-//    }
-//      for (let i = 1; i < 9; i++) {
-//        const square6 = document.createElement("div");
-//        if (i % 2 === 0) {
-//          square6.style.backgroundColor = "green";
-//        } else {
-//          square6.style.backgroundColor = "white";
-//        }
-//        chessBoard.appendChild(square6);
-//      }
-//       for (let i = 1; i < 9; i++) {
-//         const square7 = document.createElement("div");
-//         if (i % 2 === 0) {
-//           square7.style.backgroundColor = "white";
-//         } else {
-//           square7.style.backgroundColor = "green";
-//         }
-//         chessBoard.appendChild(square7);
+//     const tr = document.createElement("tr");
+//     for (let j = 1; j < 9; j++) {
+//       const td = document.createElement("td");
+//       if ((i + j) % 2 === 0) {
+//         td.setAttribute("class", "cell whitecell");
+//         tr.appendChild(td);
+//       } else {
+//         td.setAttribute("class", "cell blackcell");
+//         tr.appendChild(td);
 //       }
-//      if( i<1 || i>8){
-//          return false;
-//      }
+//     }
+//     table.appendChild(tr);
+
+//   }
+//   center.appendChild(table);
+//   table.setAttribute("cellspacing", "0");
+//   table.setAttribute("width","500px");
+//   table.setAttribute("height", "500px");
+//   document.body.appendChild(center);
+
 // };
-// myBoard();
+// chessTable();
+
+// function singleTile(c, x, y, p = false, high = false) {
+//   let div = document.createElement("div");
+//   div.style.backgroundColor = c;
+//   div.style.display = "inline-block";
+//   div.style.height = "50px";
+//   div.style.width = "50px";
+//   div.setAttribute("onclick", `func(${x},${y})`);
+//   if (p) {
+//     let i = document.createElement("i");
+//     i.classList.add("fa-solid", "fa-chess-king");
+//     i.style.width = "80%";
+//     div.appendChild(i);
+//   }
+//   if (high) {
+//     div.classList.add("opacity");
+//   }
+//   return div;
+// }
+// const my = (x, y) => {
+//   let div = document.createElement("span");
+//   div.style.backgroundColor = "black";
+//   div.style.margin = "30px";
+//   let k = 1;
+//   for (let i = 1; i < 9; i++) {
+//     let row = document.createElement("div");
+//     let col = document.createElement("div");
+//     for (let j = 1; j < 9; j++) {
+//       let c = "white";
+//       if (k % 2 === 0) c = "green";
+//       if (x === i && y === j) {
+//         console.log("Condition met");
+//         col.appendChild(singleTile("red", j, i));
+//       } else {
+//         col.appendChild(singleTile(c, j, i));
+//         k++;
+//         console.log(k);
+//       }
+//     }
+//     k++;
+//     row.appendChild(col);
+//     div.appendChild(row);
+//   }
+//   return div;
+// };
+// function func(x, y) {
+//   console.log(x, y);
+// }
+"use strict";
+const chessBoard = document.querySelector(".knightBoard");
+function possibleKnightCoordinates(x = 1, y = 1) {
+  let anotherplace = [];
+  let myCoordinates = [
+    { x: x + 1, y: y + 2 },
+    { x: x + 2, y: y + 1 },
+    { x: x + 2, y: y - 1 },
+    { x: x + 1, y: y - 2 },
+    { x: x - 1, y: y - 2 },
+    { x: x - 2, y: y - 1 },
+    { x: x - 2, y: y + 1 },
+    { x: x - 1, y: y + 2 },
+  ];
+  for (let i = 0; i < myCoordinates.length; i++) {
+    anotherplace.push([myCoordinates[i].x, myCoordinates[i].y]);
+  }
+  return anotherplace;
+  console.log(possibleKnightCoordinates());
+}
+
+function cordinateExists(arr, a, b) {
+  if (a > 8 || a < 0 || b > 8 || b < 0) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === a && arr[i][1] === b) {
+      return true;
+    }
+  }
+  return false;
+}
+function singleTile(c, x, y, p = false, validCo = []) {
+  let div = document.createElement("td");
+  div.style.backgroundColor = c;
+  div.style.width = "55px";
+  div.style.height = "55px";
+  div.setAttribute("onclick", `func(${x},${y})`);
+  if (p) {
+    let i = document.createElement("i");
+    i.classList.add("fa-solid", "fa-chess-knight", "fa-3x", "damn");
+    div.appendChild(i);
+  }
+  if (cordinateExists(validCo, x, y)) {
+    div.classList.add("opacity");
+    div.setAttribute("onclick", `func(${x},${y})`);
+  }
+  // if(high){
+  //     div.classList.add("opacity")
+  // }
+  return div;
+}
+
+const my = (x, y) => {
+  let center = document.createElement("center");
+  let div = document.createElement("table");
+  let another = document.getElementById("root");
+  let k = 1;
+  var validCo = possibleKnightCoordinates(x, y);
+  console.log(validCo);
+  for (var i = 1; i < 9; i++) {
+    var row = document.createElement("tr");
+    var col = document.createElement("td");
+    for (var j = 1; j < 9; j++) {
+      let c = "white";
+      if (k % 2 === 0) c = "black";
+      if (x === i && y === j) {
+        col.appendChild(singleTile("green", i, j, true, validCo));
+      } else {
+        col.appendChild(singleTile(c, i, j, false, validCo));
+        k++;
+      }
+    }
+    k++;
+    row.appendChild(col);
+    div.appendChild(row);
+  }
+
+  center.appendChild(div);
+  another.appendChild(center);
+  return div;
+};
+
+function func(x, y) {
+  console.log(x, y);
+  let root = document.getElementById("root");
+  removeAllChildNodes(root);
+  root.appendChild(chessBoard());
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function cell(c, x, y, p = false, validco) {
+  let div = document.createElement("div");
+
+  div.classList.add("damn", "parentCont");
+
+  div.style.backgroundColor = c;
+
+  if (p) {
+    let img = document.createElement("img");
+    img.src = "./image/knight.jpg";
+    img.style.height = "100%";
+    img.setAttribute("alt", "the img");
+    div.appendChild(img);
+
+    return div;
+  }
+  if (cordinateExists(validco, x, y)) {
+    let img = document.createElement("img");
+    img.src = "./image/circle.png";
+    img.style.height = "100%";
+    img.setAttribute("alt", "the img");
+    div.appendChild(img);
+    div.setAttribute("onclick", `func(${x},${y})`);
+    return div;
+  }
+
+  return div;
+}
+
+function diag(x, y) {
+  let k = x;
+  let j = y;
+  let arr = [];
+
+  for (let i = 1; i < 16; i++) {
+    if (k < 1 || j > 8) {
+      break;
+    }
+    arr.push([k, j]);
+    k--;
+    j++;
+  }
+  return arr;
+}
+function func(x, y) {
+  console.log(x, y);
+  let root = document.getElementById("root");
+  let finale = my(x, y);
+  console.log(root.firstChild);
+  removeAllChildNodes(root);
+  root.appendChild(finale);
+}
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+function myChessBoard(x, y) {
+  let parentCont = document.createElement("div");
+  parentCont.classList.add("parentCont");
+  let cont = document.createElement("div");
+
+  let k = 1;
+  var validCo = possibleKnightCoordinates(x, y);
+  for (var i = 1; i < 9; i++) {
+    let row = document.createElement("div");
+    let col = document.createElement("div");
+    col.classList.add("cont");
+    for (var j = 1; j < 9; j++) {
+      let c = "white";
+      if (k % 2 === 0) c = "black";
+      if (x === i && y === j) {
+        col.appendChild(cell(c, i, j, true, validCo));
+      } else {
+        col.appendChild(cell(c, i, j, false, validCo));
+      }
+      k++;
+      row.appendChild(col);
+    }
+    k++;
+    cont.appendChild(row);
+  }
+  parentCont.appendChild(cont);
+  return parentCont;
+}
+
+document.getElementById("root").appendChild(myChessBoard(7, 6));
